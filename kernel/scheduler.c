ee28bf619ae475f6cb6ef1df081f78e05eeab8be
Nethmi922 <nnavodya922@gmail.com>
2026-09-16 18:31:50 +0530

    Fix timer EOI and Stage 4 runtime startup

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -1,5 +1,9 @@
 #include "process.h"
 
+static inline void outb(uint16_t port, uint8_t value) {
+    __asm__ __volatile__("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
 uint32_t scheduler_tick(uint32_t *saved_esp) {
     proc_table[current_proc].esp = (uint32_t)saved_esp;
     proc_table[current_proc].ticks++;
@@ -10,10 +14,14 @@ uint32_t scheduler_tick(uint32_t *saved_esp) {
         next = (next + 1) % MAX_PROCS;
         searched++;
     }
-    if (searched == MAX_PROCS) return (uint32_t)saved_esp;
+    if (searched == MAX_PROCS) {
+        outb(0x20, 0x20);
+        return (uint32_t)saved_esp;
+    }
 
     proc_table[current_proc].state = PROC_READY;
     proc_table[next].state = PROC_RUNNING;
     current_proc = next;
+    outb(0x20, 0x20);
     return proc_table[next].esp;
 }
